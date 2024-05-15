351f80985f67b8da17ac63d6dae19925d5e00b5a
shivanidrabla <shivanidrabla@gmail.com>
2024-05-15 07:17:49 +0530

    Values of different datatypes

---
+++ Datatypes.py
@@ -0,0 +1,32 @@
+int=5
+float=5.2
+complex=2+4j
+dict={"a":1,"b":2,"c":3}
+bool=True
+set={"abc",40}
+str="Python is easy"
+list=["Green","Orange","Blue"]
+tuple=(0,1,2,3)
+range=range(6)
+frozenset=frozenset({"apple", "banana", "cherry"})
+byte=bytes(4)
+bytearray=bytearray(5)
+memoryview=memoryview(bytes(5))
+none=None
+print(int,type(int))
+print(float,type(float))
+print(complex,type(complex))
+print(dict,type(dict))
+print(bool,type(bool))
+print(set,type(set))
+print(str,type(str))
+print(list,type(list))
+print(tuple,type(tuple))
+print(range,type(range))
+print(frozenset,type(frozenset))
+print(byte,type(byte))
+print(bytearray,type(bytearray))
+print(memoryview,type(memoryview))
+print(none,type(none))
+
+
